87c1e9f293369a1db06661d48d4d5b46ab4d2618
nkcuong2761 <nkcuong2761@gmail.com>
2019-06-23 15:58:33 +0700

    test

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -37,6 +37,11 @@ public class Rectangle {
                 && other.top() <= this.bot();
     }
 
+
+
+
+
+
 //    public static void main(String[] args) {
 //        Rectangle r1 = new Rectangle(3, 3, 5, 5);
 //        Rectangle r2 = new Rectangle(4, 4, 5, 5);
